83f881f8128e626883992737ce0cd6940663b8d8
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-03-16 19:06:10 -0300

    feat: create welcome message ande search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,24 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel</h2>
+        <p className="text-sm capitalize">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+
+        <div className="mt-6 px-3">
+          <Search />
+        </div>
+      </div>
+    </>
+  );
 }

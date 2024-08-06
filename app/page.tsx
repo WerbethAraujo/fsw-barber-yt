96269dcb8e84833591b8dc638c9cba1d1a92d8b4
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-08-05 23:49:27 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="mb-5 text-[36px] text-red-600">Homepage</h1>
+      <Button>Iniciar</Button>
     </main>
   )
 }

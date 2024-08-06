57adad2d974d07c75c2a89d09e47826ecbea4c71
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-08-05 23:54:55 -0300

    chore: update shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "./_components/ui/button"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="mb-5 text-[36px] text-red-600">Homepage</h1>
+      <h1 className="mb-5 text-[36px] uppercase text-purple-500">Homepage</h1>
       <Button>Iniciar</Button>
     </main>
   )

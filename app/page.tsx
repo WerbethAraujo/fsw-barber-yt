00d6c25e42dee70c0bb0955d6fb62548cf387cb4
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-08-08 20:20:03 -0300

    feat: add footer component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
-import { Card, CardContent } from "./_components/ui/card"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershopItem"
 import { quickSearchItems } from "./_constants/quickSearchItems"
@@ -71,16 +70,6 @@ export default async function Home() {
           ))}
         </div>
       </div>
-      <footer>
-        <Card>
-          <CardContent className="px-5 py-6">
-            <p className="text-sm text-gray-400">
-              &copy; 2024 Copyright{" "}
-              <span className="font-bold">FSW barber</span>
-            </p>
-          </CardContent>
-        </Card>
-      </footer>
     </>
   )
 }

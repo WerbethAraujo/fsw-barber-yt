90c21990a8036f6266922185626b01fe9b1a7fec
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-08-10 17:46:38 -0300

    feat: add next auth setup

--- app/_components/sidebarSheet.tsx
+++ app/_components/sidebarSheet.tsx
@@ -2,9 +2,17 @@ import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
 import Link from "next/link"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { quickSearchItems } from "../_constants/quickSearchItems"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -12,15 +20,41 @@ const SidebarSheet = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
+      <h2>Ola, Faça o seu login</h2>
 
-      <div className="flex items-center gap-3 border-b border-solid p-5">
-        <Avatar className="flex justify-start gap-2">
+      <div className="flex items-center justify-between gap-3 border-b border-solid p-5">
+        <h2 className="font-bold">Olá, faça seu login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon size={18} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-2 font-bold">
+              <Image
+                src="/google.svg"
+                width={18}
+                height={18}
+                alt="logo google"
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar className="flex justify-start gap-2">
           <AvatarImage src="/avatar.jpg" />
         </Avatar>
         <div>
           <p className="font-semibold">Anthony Gabriel</p>
           <p className="text-xs">anthonygabriel@gmail.com</p>
-        </div>
+        </div> */}
       </div>
       <div className="flex flex-col gap-2 border-b border-solid p-5">
         <SheetClose asChild>
